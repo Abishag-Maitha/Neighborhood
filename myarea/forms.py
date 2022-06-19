022a6d07cab979ba29be9a76354bafdd82060890
Abishag-Maitha <abishag.maitha@student.moringaschool.com>
2022-06-19 14:09:32 +0300

    Add Profile and registration forms

--- myarea/forms.py
+++ myarea/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from .models import *
+from django.contrib.auth.forms import UserCreationForm
 
 
 class NewPostForm(forms.ModelForm):
@@ -29,3 +30,22 @@ class BusinessForm(forms.ModelForm):
     class Meta:
         model  = Business
         fields ='__all__'
+
+class RegistrationForm(UserCreationForm):
+    email=forms.EmailField()
+    class Meta:
+        model = User
+        fields = ['username', 'email','password1', 'password2']
+
+    def save(self, commit=True):
+        user=super().save(commit=False)
+        user.email=self.cleaned_data['email']
+        if commit:
+
+            user.save()
+        return user
+
+class profileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = [ 'photo', 'Bio']
